91d2eab78824ec08c60caac23320858cbd640319
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2022-10-26 10:31:12 -0300

    Add AS IS BASIS on examples files

--- examples/OneLedClock/OneLedClock.ino
+++ examples/OneLedClock/OneLedClock.ino
@@ -9,6 +9,11 @@
  * The Pisco-LED-Code library is a nom blocking function that 
  * should be called frequently from the loop function. 
  * 
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ * 
  * Andre Viegas
  */
 
