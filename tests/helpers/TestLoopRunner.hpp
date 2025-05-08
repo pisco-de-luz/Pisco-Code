b320350ded4a15c346fdb9cb3495baa6e1a34eed
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-05-08 17:01:07 +0200

    Refactor runSequencer to use CodeBlinker and remove deprecated version

--- tests/helpers/TestLoopRunner.hpp
+++ tests/helpers/TestLoopRunner.hpp
@@ -2,15 +2,15 @@
 #define TEST_LOOP_RUNNER_HPP
 
 #include "../mocks/MockLedControlLogger.hpp"
-#include "Pisco-Code.hpp"
 #include "code_blinker.hpp"
+#include "pisco_constants.hpp"
 #include <cstdint>
 
 namespace testutils
 {
 
     // Drives the loop for a maximum simulated time, default 64 seconds.
-    inline void runSequencer(PiscoCode& code, MockLedControlLogger* logger)
+    inline void runSequencer(pisco::CodeBlinker* code, MockLedControlLogger* logger)
     {
         constexpr uint8_t   LOOP_CALLS_PER_COUNTER = 64;
         constexpr Timestamp MAX_SEQUENCING_LOOP =
@@ -18,35 +18,14 @@ namespace testutils
         Timestamp loopCounter = 0;
         uint8_t   fakeMillis  = 0;
 
-        while (code.isSequencing() && loopCounter < MAX_SEQUENCING_LOOP)
+        while (code->isRunning() && loopCounter < MAX_SEQUENCING_LOOP)
         {
             for (uint8_t i = 0; i < LOOP_CALLS_PER_COUNTER; ++i)
             {
                 logger->setTime(loopCounter++);
-                code.loop(fakeMillis);
+                code->loop(fakeMillis);
             }
 
-            ++fakeMillis;
-        }
-
-        logger->flush();
-    }
-
-    // New version compatible with CodeBlinker
-    inline void runSequencer(pisco::CodeBlinker* blinker, MockLedControlLogger* logger)
-    {
-        constexpr uint8_t  LOOP_CALLS_PER_COUNTER = 64;
-        constexpr uint32_t MAX_SEQUENCING_LOOP    = 64000UL;
-        uint32_t           loopCounter            = 0;
-        uint8_t            fakeMillis             = 0;
-
-        while (blinker->isRunning() && loopCounter < MAX_SEQUENCING_LOOP)
-        {
-            for (uint8_t i = 0; i < LOOP_CALLS_PER_COUNTER; ++i)
-            {
-                logger->setTime(loopCounter++);
-                blinker->loop(fakeMillis);
-            }
             ++fakeMillis;
         }
 
