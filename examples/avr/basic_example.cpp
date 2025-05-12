1635d0fb190d4d3b4ac0861b43800ec14c8a42a3
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-05-12 10:15:00 +0200

    Update showCode call in main to use correct code value for LED blinking

--- examples/avr/basic_example.cpp
+++ examples/avr/basic_example.cpp
@@ -59,7 +59,7 @@ int main()
 
     uint32_t fakeMillis = 3660000UL; // 1 hour, 1 minute, 0 seconds
 
-    blinker.showCode(13, base_t::DECIMAL, 0, 3);
+    blinker.showCode(102, base_t::DECIMAL, 0, 3);
     while (blinker.isRunning())
     {
         blinker.loop(fakeMillis++ >> 6);
