f4d26890d7504f70d8b32e38f41d31178d0f729c
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-05-15 11:27:44 +0200

    Fix digit calculation in runSameDigitsUpToMaxDigitsForBase function for correct base handling

--- tests/helpers/test_patterns_common.hpp
+++ tests/helpers/test_patterns_common.hpp
@@ -13,12 +13,14 @@ inline void runSameDigitsUpToMaxDigitsForBase(pisco::NumberBase base, pisco::Cod
     for (pisco::NumDigits num_digits = 1; num_digits <= pisco::MAX_DIGITS; ++num_digits)
     {
         logger.clear();
-        const pisco::DigitValue digit =
-            ((pisco::MAX_DIGITS - num_digits) % (to_value(base) - 1)) + 1;
+        const pisco::DigitValue digit = ((to_value(base) - num_digits) % (to_value(base)));
 
         const pisco::BlinkCode code = testutils::generatePatternOfDigits(
             {testutils::PatternOption::SameDigit, base, num_digits, digit});
 
+        // show code in decimal and hexadecimal
+        // std::cout << "code: " << std::dec << code << " (decimal), " << std::hex << code
+        //           << " (hexadecimal)" << std::endl;
         const testutils::TestBlinkerCase test_case{
             .blink_code  = code,
             .number_base = base,
