4b88e04ec51737ba38a1655053a466db136aa198
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-05-15 11:27:50 +0200

    Add hexadecimal systematic high-level tests for digit blinking functionality

---
+++ tests/hexadecimal_systematic_high_level_tests.cpp
@@ -0,0 +1,30 @@
+#include "CppUTest/TestHarness.h"
+
+#include <cstdint>
+#include <iostream>
+
+#include "code_blinker.hpp"
+#include "helpers/BlinkerTestUtils.hpp"
+#include "helpers/test_patterns_common.hpp"
+#include "mocks/MockLedControlLogger.hpp"
+#include "mocks/MockLedControllerAdapter.hpp"
+#include "pisco_constants.hpp"
+#include "pisco_types.hpp"
+#include "tests_constants.hpp"
+#include "tests_types.hpp"
+
+using namespace pisco;
+using testutils::runSequencer;
+
+TEST_GROUP(HexadecimalSystematicHighLevelTests)
+{
+    MockLedControlLogger     logger;
+    MockLedControllerAdapter controller{&logger};
+    CodeBlinker              blinker{&controller};
+};
+
+// - Example: 9, 88, 777, ..., 111111111
+TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSameDigitsUpToMaxDigits)
+{
+    runSameDigitsUpToMaxDigitsForBase(NumberBase::HEXADECIMAL, blinker, logger);
+}
