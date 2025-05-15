feddb175813d40cb55bdfb3ec2ebec8cd45b410c
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-05-15 11:27:13 +0200

    Update MAX_DIGITS constant to 8 for 32-bit integer representation

--- include/pisco_constants.hpp
+++ include/pisco_constants.hpp
@@ -36,7 +36,7 @@ namespace pisco
     }
 
     // Maximum number of digits limited to 9 for 32-bit integer representation
-    constexpr NumDigits MAX_DIGITS = 9;
+    constexpr NumDigits MAX_DIGITS = 8;
 
     constexpr uint8_t  DEFAULT_DIMMED_LEVEL = 3;
     constexpr uint8_t  PWM_MAX              = 15;
