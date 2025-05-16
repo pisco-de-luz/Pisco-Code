ee2d3a9c44a6ca48155ba23f09a61692787f503d
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-05-16 17:14:13 +0200

    Remove testutils namespace usage

--- tests/setter_behavior_blinker_test.cpp
+++ tests/setter_behavior_blinker_test.cpp
@@ -9,7 +9,7 @@
 #include "pisco_types.hpp"
 
 using namespace pisco_code;
-using testutils::runSequencer;
+using namespace testutils;
 
 TEST_GROUP(SetterBehaviorBlinkerTest)
 {
@@ -20,172 +20,172 @@ TEST_GROUP(SetterBehaviorBlinkerTest)
 
 TEST(SetterBehaviorBlinkerTest, ShouldUseDefaultPwmLevel)
 {
-    const testutils::TestBlinkerCase test_case{
-        .trace_check   = testutils::TraceCheck::NotEnforced,
+    const TestBlinkerCase test_case{
+        .trace_check   = TraceCheck::NotEnforced,
         .expectedPulse = DEFAULT_PULSE_LEVEL,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldUseCustomPwmLevel)
 {
 
-    blinker.setPeakLevel(testutils::MID_PULSE_LEVEL);
-    const testutils::TestBlinkerCase test_case{
-        .trace_check   = testutils::TraceCheck::NotEnforced,
-        .expectedPulse = testutils::MID_PULSE_LEVEL,
+    blinker.setPeakLevel(MID_PULSE_LEVEL);
+    const TestBlinkerCase test_case{
+        .trace_check   = TraceCheck::NotEnforced,
+        .expectedPulse = MID_PULSE_LEVEL,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldRejectTooHighPwmLevel)
 {
-    blinker.setPeakLevel(testutils::TOO_HIGH_PULSE_LEVEL);
-    const testutils::TestBlinkerCase test_case{
-        .trace_check   = testutils::TraceCheck::NotEnforced,
-        .expectedPulse = testutils::HIGHEST_PULSE_LEVEL,
+    blinker.setPeakLevel(TOO_HIGH_PULSE_LEVEL);
+    const TestBlinkerCase test_case{
+        .trace_check   = TraceCheck::NotEnforced,
+        .expectedPulse = HIGHEST_PULSE_LEVEL,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldRejectSlightHighPwmLevel)
 {
-    blinker.setPeakLevel(testutils::HIGHEST_PULSE_LEVEL + 1);
-    const testutils::TestBlinkerCase test_case{
-        .trace_check   = testutils::TraceCheck::NotEnforced,
-        .expectedPulse = testutils::HIGHEST_PULSE_LEVEL,
+    blinker.setPeakLevel(HIGHEST_PULSE_LEVEL + 1);
+    const TestBlinkerCase test_case{
+        .trace_check   = TraceCheck::NotEnforced,
+        .expectedPulse = HIGHEST_PULSE_LEVEL,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldUseDefaultDimmedLevel)
 {
-    const testutils::TestBlinkerCase test_case{
-        .trace_check    = testutils::TraceCheck::NotEnforced,
+    const TestBlinkerCase test_case{
+        .trace_check    = TraceCheck::NotEnforced,
         .expectedDimmed = DEFAULT_DIMMED_LEVEL,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldUseCustomDimmedLevel)
 {
 
-    blinker.setDimmedLevel(testutils::MID_DIMMED_LEVEL);
-    const testutils::TestBlinkerCase test_case{
-        .trace_check    = testutils::TraceCheck::NotEnforced,
-        .expectedDimmed = testutils::MID_DIMMED_LEVEL,
+    blinker.setDimmedLevel(MID_DIMMED_LEVEL);
+    const TestBlinkerCase test_case{
+        .trace_check    = TraceCheck::NotEnforced,
+        .expectedDimmed = MID_DIMMED_LEVEL,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 // Expect dimmed level to be clamped to safe default when above allowed maximum.
 TEST(SetterBehaviorBlinkerTest, ShouldRejectTooHighDimmedLevel)
 {
-    blinker.setDimmedLevel(testutils::TOO_HIGH_DIMMED_LEVEL);
-    const testutils::TestBlinkerCase test_case{
-        .trace_check    = testutils::TraceCheck::NotEnforced,
-        .expectedDimmed = testutils::HIGHEST_DIMMED_LEVEL,
+    blinker.setDimmedLevel(TOO_HIGH_DIMMED_LEVEL);
+    const TestBlinkerCase test_case{
+        .trace_check    = TraceCheck::NotEnforced,
+        .expectedDimmed = HIGHEST_DIMMED_LEVEL,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldNotAffectDefaultPulseLevelWhenSettingDimmedLevel)
 {
-    for (auto led_level : testutils::ALL_DIMMED_LEVELS)
+    for (auto led_level : ALL_DIMMED_LEVELS)
     {
         blinker.setDimmedLevel(led_level);
 
-        const testutils::TestBlinkerCase test_case{
-            .trace_check   = testutils::TraceCheck::NotEnforced,
+        const TestBlinkerCase test_case{
+            .trace_check   = TraceCheck::NotEnforced,
             .expectedPulse = DEFAULT_PULSE_LEVEL,
         };
 
-        testutils::checkBlinkerBehavior(blinker, logger, test_case);
+        checkBlinkerBehavior(blinker, logger, test_case);
     }
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldNotAffecLowestDimmedLevelWhenSettingPulseLevel)
 {
-    for (auto led_level : testutils::ALL_PULSE_LEVELS)
+    for (auto led_level : ALL_PULSE_LEVELS)
     {
         blinker.setPeakLevel(led_level);
-        blinker.setDimmedLevel(testutils::LOWEST_DIMMED_LEVEL);
-        const testutils::TestBlinkerCase test_case{
-            .trace_check    = testutils::TraceCheck::NotEnforced,
-            .expectedDimmed = testutils::LOWEST_DIMMED_LEVEL,
+        blinker.setDimmedLevel(LOWEST_DIMMED_LEVEL);
+        const TestBlinkerCase test_case{
+            .trace_check    = TraceCheck::NotEnforced,
+            .expectedDimmed = LOWEST_DIMMED_LEVEL,
         };
 
-        testutils::checkBlinkerBehavior(blinker, logger, test_case);
+        checkBlinkerBehavior(blinker, logger, test_case);
     }
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldPadWithLeadingZeros)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_0010,
-        .trace_check = testutils::TraceCheck::Enforced,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_0010,
+        .trace_check = TraceCheck::Enforced,
         .numDigits   = 4,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldNotPadIfNotNeeded)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_12345,
-        .trace_check = testutils::TraceCheck::Enforced,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_12345,
+        .trace_check = TraceCheck::Enforced,
         .numDigits   = 5,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldTruncateToNumDigits)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_12345_TRUNCATED_TO_2_DIGITS,
-        .trace_check = testutils::TraceCheck::Enforced,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_12345_TRUNCATED_TO_2_DIGITS,
+        .trace_check = TraceCheck::Enforced,
         .numDigits   = 2,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldRejectTooHighNumDigits)
 {
-    const NumDigits                  max_digits = maxDigitsForBase(NumberBase::DEC);
-    const testutils::TestBlinkerCase test_case{
-        .trace_check = testutils::TraceCheck::Enforced,
+    const NumDigits       max_digits = maxDigitsForBase(NumberBase::DEC);
+    const TestBlinkerCase test_case{
+        .trace_check = TraceCheck::Enforced,
         .numDigits   = max_digits + 1,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldRejectTooLowNumDigits)
 {
-    const testutils::TestBlinkerCase test_case{
-        .trace_check = testutils::TraceCheck::Enforced,
+    const TestBlinkerCase test_case{
+        .trace_check = TraceCheck::Enforced,
         .numDigits   = 0,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(SetterBehaviorBlinkerTest, ShouldHandleSingleDigitZero)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_0,
-        .trace_check = testutils::TraceCheck::Enforced,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_0,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
