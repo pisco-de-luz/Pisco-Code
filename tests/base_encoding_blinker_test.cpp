a45d16ed9ed16a5a83e0218e24a3c70bf904bc23
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-05-16 17:09:51 +0200

    Remove testutils namespace usage and simplify test case definitions

--- tests/base_encoding_blinker_test.cpp
+++ tests/base_encoding_blinker_test.cpp
@@ -5,9 +5,12 @@
 #include "mocks/mock_led_control_logger.hpp"
 #include "mocks/mock_led_controller_adapter.hpp"
 #include "pisco_constants.hpp"
+#include "pisco_types.hpp"
+#include "tests_constants.hpp"
+#include "tests_types.hpp"
 
 using namespace pisco_code;
-using testutils::runSequencer;
+using namespace testutils;
 
 TEST_GROUP(BaseEncodingBlinkerTest)
 {
@@ -18,77 +21,77 @@ TEST_GROUP(BaseEncodingBlinkerTest)
 
 TEST(BaseEncodingBlinkerTest, CanShowDecimal10)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_10,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_10,
         .number_base = NumberBase::DEC,
-        .trace_check = testutils::TraceCheck::Enforced,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(BaseEncodingBlinkerTest, CanShowZero)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_0,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_0,
         .number_base = NumberBase::DEC,
-        .trace_check = testutils::TraceCheck::Enforced,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(BaseEncodingBlinkerTest, CanShowDecimal12345)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_12345,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_12345,
         .number_base = NumberBase::DEC,
-        .trace_check = testutils::TraceCheck::Enforced,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(BaseEncodingBlinkerTest, CanShowBinary5)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_BIN_5,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_BIN_5,
         .number_base = NumberBase::BIN,
-        .trace_check = testutils::TraceCheck::Enforced,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(BaseEncodingBlinkerTest, CanShowBinaryNeg7)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_BIN_NEG_7,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_BIN_NEG_7,
         .number_base = NumberBase::BIN,
-        .trace_check = testutils::TraceCheck::Enforced,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(BaseEncodingBlinkerTest, CanShowHex255)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_HEX_255,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_HEX_255,
         .number_base = NumberBase::HEX,
-        .trace_check = testutils::TraceCheck::Enforced,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
 
 TEST(BaseEncodingBlinkerTest, CanShowOctal28)
 {
-    const testutils::TestBlinkerCase test_case{
-        .blink_code  = testutils::CODE_OCT_28,
+    const TestBlinkerCase test_case{
+        .blink_code  = CODE_OCT_28,
         .number_base = NumberBase::OCT,
-        .trace_check = testutils::TraceCheck::Enforced,
+        .trace_check = TraceCheck::Enforced,
     };
 
-    testutils::checkBlinkerBehavior(blinker, logger, test_case);
+    checkBlinkerBehavior(blinker, logger, test_case);
 }
