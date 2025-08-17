765c88dc12a7e1d6c51a60d1b7cb7cfc3e709856
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-08-17 08:14:34 +0200

    Add SIGNAL_ELEMENT constexpr for DIGIT and BETWEEN PEAK signals

--- include/signal_element.hpp
+++ include/signal_element.hpp
@@ -11,14 +11,15 @@ namespace pisco_code
     struct SignalElement
     {
       public:
-        constexpr SignalElement() noexcept
-            : level_(to_value(SignalLevel::NOT_DEFINED)), times_(0),
-              duration_(to_value(SignalDuration::SHORT))
+        constexpr SignalElement() noexcept :
+            level_(to_value(SignalLevel::NOT_DEFINED)), times_(0),
+            duration_(to_value(SignalDuration::SHORT))
         {
         }
 
-        constexpr SignalElement(SignalLevel lvl, Counter cnt, SignalDuration dur) noexcept
-            : level_(to_value(lvl)), times_(cnt), duration_(to_value(dur))
+        constexpr SignalElement(SignalLevel lvl, Counter cnt,
+                                SignalDuration dur) noexcept :
+            level_(to_value(lvl)), times_(cnt), duration_(to_value(dur))
         {
         }
 
@@ -36,20 +37,26 @@ namespace pisco_code
         }
 
       private:
-        SignalLevelType    level_ : 2;    // Encoded SignalLevel
-        SignalTimesType    times_ : 4;    // How many times this unit repeats (0–15)
+        SignalLevelType level_ : 2; // Encoded SignalLevel
+        SignalTimesType times_ : 4; // How many times this unit repeats (0–15)
         SignalDurationType duration_ : 2; // Encoded SignalDuration
     };
 
-    inline constexpr SignalElement SIGNAL_ELEMENT_NEGATIVE_PEAK{SignalLevel::PEAK, 1,
-                                                                SignalDuration::LONG};
-    inline constexpr SignalElement SIGNAL_ELEMENT_ZERO_GAP{SignalLevel::GAP, 1,
-                                                           SignalDuration::SHORT};
-    inline constexpr SignalElement SIGNAL_ELEMENT_NOT_DEFINED{SignalLevel::NOT_DEFINED, 0,
-                                                              SignalDuration::SHORT};
-    constexpr auto                 signal_element_digit_peak(SignalTimesType digit_value) noexcept
+    inline constexpr SignalElement SIGNAL_ELEMENT_NEGATIVE_PEAK{
+        SignalLevel::PEAK, 1, SignalDuration::LONG};
+    inline constexpr SignalElement SIGNAL_ELEMENT_ZERO_GAP{
+        SignalLevel::GAP, 1, SignalDuration::SHORT};
+    inline constexpr SignalElement SIGNAL_ELEMENT_NOT_DEFINED{
+        SignalLevel::NOT_DEFINED, 0, SignalDuration::SHORT};
+    inline constexpr SignalElement SIGNAL_ELEMENT_BETWEEN_PEAK{
+        SignalLevel::MIDDLE, 1, SignalDuration::SHORT};
+    inline constexpr SignalElement SIGNAL_ELEMENT_DIGIT_PEAK{
+        SignalLevel::PEAK, 1, SignalDuration::SHORT};
+    constexpr auto
+    signal_element_digit_peak(SignalTimesType digit_value) noexcept
     {
-        return SignalElement(SignalLevel::PEAK, digit_value, SignalDuration::SHORT);
+        return SignalElement(SignalLevel::PEAK, digit_value,
+                             SignalDuration::SHORT);
     }
 
 } // namespace pisco_code
