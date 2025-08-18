4f2c13086a82d8725b0b22e8b7cf13638acb212f
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-08-18 14:08:42 +0200

    Add SignalPulseIterator class and corresponding tests for iteration functionality

---
+++ include/signal_pulse_iterator.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "pisco_constants.hpp"
+#include "pisco_types.hpp"
+#include "signal_element.hpp"
+#include "signal_stack.hpp"
+#include "signal_types.hpp"
+
+namespace pisco_code
+{
+    class SignalPulseIterator
+    {
+      public:
+        explicit SignalPulseIterator(SignalStack signal_stack) noexcept :
+            signal_stack_(signal_stack), remaining_(signal_stack.size())
+        {
+        }
+
+        [[nodiscard]] static SignalElement next() noexcept
+        {
+            if (!hasNext())
+            {
+                return SIGNAL_ELEMENT_NOT_DEFINED;
+            }
+            return SIGNAL_ELEMENT_ZERO_GAP;
+            // return {element_.get_level(), Byte{1}, element_.get_duration()};
+        }
+
+        [[nodiscard]] static bool hasNext() noexcept
+        {
+            return true;
+            // return remaining_ > 0 || is_pulse_between_;
+        }
+
+        void reset() noexcept
+        {
+            // remaining_        = to_value(element_.get_times());
+            // is_pulse_between_ = false;
+        }
+
+      private:
+        void advance() noexcept
+        {
+            // if (isPeakShort() && remaining_ > 1)
+            // {
+            //     is_pulse_between_ = true;
+            // }
+            // --remaining_;
+        }
+
+        SignalStack signal_stack_;
+        Counter     remaining_{0};
+        bool        is_pulse_between_{false};
+        // [[nodiscard]] bool isPeakShort() const noexcept
+        // {
+        //     return signal_stack_.get_level() == SignalLevel::PEAK &&
+        //            element_.get_duration() == SignalDuration::SHORT;
+        // }
+    };
+    constexpr auto MAX_BYTES_PASS_BY_VALUE = 16;
+
+    static_assert(sizeof(SignalStack) <= MAX_BYTES_PASS_BY_VALUE,
+                  "Stack grew, reconsider pass-by-value");
+
+} // namespace pisco_code
---
+++ tests/signal_pulse_iterator_test.cpp
@@ -0,0 +1,48 @@
+#include "CppUTest/TestHarness.h"
+
+#include "pisco_constants.hpp"
+#include "pisco_types.hpp"
+#include "signal_element.hpp"
+#include "signal_pulse_iterator.hpp"
+#include "signal_stack.hpp"
+#include "signal_types.hpp"
+#include <type_traits>
+
+using namespace pisco_code;
+
+static_assert(std::is_trivially_copyable<SignalStack>::value,
+              "Copy must stay cheap and predictable");
+
+TEST_GROUP(SignalPulseIteratorTests)
+{
+    // void check_next(const SignalElement& expected)
+    // {
+    //     const auto actual = sequence_stack.pop();
+    //     CHECK_EQUAL(to_value(expected.get_level()),
+    //                 to_value(actual.get_level()));
+    //     CHECK_EQUAL(to_value(expected.get_times()),
+    //                 to_value(actual.get_times()));
+    //     CHECK_EQUAL(to_value(expected.get_duration()),
+    //                 to_value(actual.get_duration()));
+    // }
+
+    SignalStack signal_stack;
+
+    void teardown() override
+    {
+        signal_stack.clear();
+    }
+};
+
+TEST(SignalPulseIteratorTests, NumberOne_IteratesCorrectly)
+{
+    const SignalElement element = signal_element_digit_peak(1);
+    signal_stack.push(element);
+    SignalPulseIterator iterator(signal_stack);
+
+    CHECK_TRUE(iterator.hasNext());
+    const auto expected_gap = SIGNAL_ELEMENT_FRAMING;
+    const auto actual       = iterator.next();
+    CHECK_EQUAL(to_value(expected_gap.get_level()),
+                to_value(actual.get_level()));
+}
