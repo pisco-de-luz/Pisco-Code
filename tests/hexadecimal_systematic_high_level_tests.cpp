4f2100cc7444f16bb2d72ed0f0dd06702bf41a42
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-08-25 22:59:36 +0200

    Remove unused header definitions

--- tests/hexadecimal_systematic_high_level_tests.cpp
+++ tests/hexadecimal_systematic_high_level_tests.cpp
@@ -1,17 +1,12 @@
 #include "CppUTest/TestHarness.h"
 
-#include <cstdint>
-#include <iostream>
-
 #include "helpers/blinker_test_utils.hpp"
 #include "helpers/test_patterns_common.hpp"
 #include "mocks/mock_led_control_logger.hpp"
 #include "mocks/mock_led_controller_adapter.hpp"
 #include "pisco_constants.hpp"
-#include "pisco_types.hpp"
 #include "signal_emitter.hpp"
 #include "tests_constants.hpp"
-#include "tests_types.hpp"
 
 using namespace pisco_code;
 
@@ -27,40 +22,52 @@ TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSameDigitsUpToMaxDigits)
     runSameDigitsUpToMaxDigitsForBase(NumberBase::HEX, blinker, logger);
 }
 
-TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSequentialUpDigitsUpToMaxDigits)
+TEST(HexadecimalSystematicHighLevelTests,
+     ShouldBlinkSequentialUpDigitsUpToMaxDigits)
 {
     runSequentialUpDigitsUpToMaxDigitsForBase(NumberBase::HEX, blinker, logger);
 }
 
-TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSequentialDownDigitsUpToMaxDigits)
+TEST(HexadecimalSystematicHighLevelTests,
+     ShouldBlinkSequentialDownDigitsUpToMaxDigits)
 {
-    runSequentialDownDigitsUpToMaxDigitsForBase(NumberBase::HEX, blinker, logger);
+    runSequentialDownDigitsUpToMaxDigitsForBase(NumberBase::HEX, blinker,
+                                                logger);
 }
 
-TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSequentialDigitsUpToMaxDigitsPaddedToMaxDigits)
+TEST(HexadecimalSystematicHighLevelTests,
+     ShouldBlinkSequentialDigitsUpToMaxDigitsPaddedToMaxDigits)
 {
-    runSequentialDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::HEX, blinker, logger);
+    runSequentialDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::HEX,
+                                                             blinker, logger);
 }
 
-TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSameDigitsUpToMaxDigitsPaddedToMaxDigits)
+TEST(HexadecimalSystematicHighLevelTests,
+     ShouldBlinkSameDigitsUpToMaxDigitsPaddedToMaxDigits)
 {
-    runSameDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::HEX, blinker, logger);
+    runSameDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::HEX, blinker,
+                                                       logger);
 }
 
 TEST(HexadecimalSystematicHighLevelTests,
      ShouldBlinkSequentialDigitsUpToMaxDigitsPaddedToHalfMaxDigits)
 {
-    runSequentialDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(NumberBase::HEX, blinker, logger);
+    runSequentialDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(
+        NumberBase::HEX, blinker, logger);
 }
 
-TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSameDigitsUpToMaxDigitsPaddedToHalfMaxDigits)
+TEST(HexadecimalSystematicHighLevelTests,
+     ShouldBlinkSameDigitsUpToMaxDigitsPaddedToHalfMaxDigits)
 {
-    runSameDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(NumberBase::HEX, blinker, logger);
+    runSameDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(NumberBase::HEX,
+                                                           blinker, logger);
 }
 
 // Ignored due to long runtime from many full-digit blink sequences.
 // Useful for stress/regression tests, not routine execution.
-IGNORE_TEST(HexadecimalSystematicHighLevelTests, ShouldBlinkSameMaxBaseDigitUpToMaxDigitsNineTimes)
+IGNORE_TEST(HexadecimalSystematicHighLevelTests,
+            ShouldBlinkSameMaxBaseDigitUpToMaxDigitsNineTimes)
 {
-    runSameMaxBaseDigitUpToMaxDigitsNineTimesForBase(NumberBase::HEX, blinker, logger);
+    runSameMaxBaseDigitUpToMaxDigitsNineTimesForBase(NumberBase::HEX, blinker,
+                                                     logger);
 }
