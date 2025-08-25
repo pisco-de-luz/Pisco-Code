0a0ebb4e9ff9ac689d9499c90bfcae261aa61f23
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-08-25 22:58:39 +0200

    Remove unused header definitions

--- tests/decimal_systematic_high_level_tests.cpp
+++ tests/decimal_systematic_high_level_tests.cpp
@@ -1,17 +1,12 @@
 #include "CppUTest/TestHarness.h"
 
-#include <cstdint>
-#include <iostream>
-
 #include "helpers/blinker_test_utils.hpp"
 #include "helpers/test_patterns_common.hpp"
 #include "mocks/mock_led_control_logger.hpp"
 #include "mocks/mock_led_controller_adapter.hpp"
 #include "pisco_constants.hpp"
-#include "pisco_types.hpp"
 #include "signal_emitter.hpp"
 #include "tests_constants.hpp"
-#include "tests_types.hpp"
 
 using namespace pisco_code;
 
@@ -27,39 +22,52 @@ TEST(DecimalSystematicHighLevelTests, ShouldBlinkSameDigitsUpToMaxDigits)
     runSameDigitsUpToMaxDigitsForBase(NumberBase::DEC, blinker, logger);
 }
 
-TEST(DecimalSystematicHighLevelTests, ShouldBlinkSequentialUpDigitsUpToMaxDigits)
+TEST(DecimalSystematicHighLevelTests,
+     ShouldBlinkSequentialUpDigitsUpToMaxDigits)
 {
     runSequentialUpDigitsUpToMaxDigitsForBase(NumberBase::DEC, blinker, logger);
 }
 
-TEST(DecimalSystematicHighLevelTests, ShouldBlinkSequentialDownDigitsUpToMaxDigits)
+TEST(DecimalSystematicHighLevelTests,
+     ShouldBlinkSequentialDownDigitsUpToMaxDigits)
 {
-    runSequentialDownDigitsUpToMaxDigitsForBase(NumberBase::DEC, blinker, logger);
+    runSequentialDownDigitsUpToMaxDigitsForBase(NumberBase::DEC, blinker,
+                                                logger);
 }
 
-TEST(DecimalSystematicHighLevelTests, ShouldBlinkSequentialDigitsUpToMaxDigitsPaddedToMaxDigits)
+TEST(DecimalSystematicHighLevelTests,
+     ShouldBlinkSequentialDigitsUpToMaxDigitsPaddedToMaxDigits)
 {
-    runSequentialDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::DEC, blinker, logger);
+    runSequentialDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::DEC,
+                                                             blinker, logger);
 }
 
-TEST(DecimalSystematicHighLevelTests, ShouldBlinkSameDigitsUpToMaxDigitsPaddedToMaxDigits)
+TEST(DecimalSystematicHighLevelTests,
+     ShouldBlinkSameDigitsUpToMaxDigitsPaddedToMaxDigits)
 {
-    runSameDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::DEC, blinker, logger);
+    runSameDigitsUpToMaxDigitsPaddedToMaxDigitsForBase(NumberBase::DEC, blinker,
+                                                       logger);
 }
 
-TEST(DecimalSystematicHighLevelTests, ShouldBlinkSequentialDigitsUpToMaxDigitsPaddedToHalfMaxDigits)
+TEST(DecimalSystematicHighLevelTests,
+     ShouldBlinkSequentialDigitsUpToMaxDigitsPaddedToHalfMaxDigits)
 {
-    runSequentialDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(NumberBase::DEC, blinker, logger);
+    runSequentialDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(
+        NumberBase::DEC, blinker, logger);
 }
 
-TEST(DecimalSystematicHighLevelTests, ShouldBlinkSameDigitsUpToMaxDigitsPaddedToHalfMaxDigits)
+TEST(DecimalSystematicHighLevelTests,
+     ShouldBlinkSameDigitsUpToMaxDigitsPaddedToHalfMaxDigits)
 {
-    runSameDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(NumberBase::DEC, blinker, logger);
+    runSameDigitsUpToMaxDigitsPaddedToHalfMaxDigitsForBase(NumberBase::DEC,
+                                                           blinker, logger);
 }
 
 // Ignored due to long runtime from many full-digit blink sequences.
 // Useful for stress/regression tests, not routine execution.
-IGNORE_TEST(DecimalSystematicHighLevelTests, ShouldBlinkSameMaxBaseDigitUpToMaxDigitsNineTimes)
+IGNORE_TEST(DecimalSystematicHighLevelTests,
+            ShouldBlinkSameMaxBaseDigitUpToMaxDigitsNineTimes)
 {
-    runSameMaxBaseDigitUpToMaxDigitsNineTimesForBase(NumberBase::DEC, blinker, logger);
+    runSameMaxBaseDigitUpToMaxDigitsNineTimesForBase(NumberBase::DEC, blinker,
+                                                     logger);
 }
