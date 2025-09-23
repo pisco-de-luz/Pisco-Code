bf7bd278eba16c084be0ad0aa748cfb088c3f6a9
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-09-23 18:19:37 +0200

    Refactor STM32 example macro function: streamline source file management, enhance linker script handling, and improve upload command clarity

--- examples/stm32/stm32_example_macros.cmake
+++ examples/stm32/stm32_example_macros.cmake
@@ -6,96 +6,77 @@
 
 enable_language(ASM)
 
-function(add_stm32_example EX_NAME BOARD BOARD_CANON)
-  set(HAL_CPP "boards/${BOARD}/hal_led_${BOARD_CANON}.cpp")
-  set(STARTUP_ASM "boards/${BOARD}/vendor/startup_stm32f410rx.s")
-  set(SYSTEM_C   "boards/${BOARD}/vendor/system_stm32f4xx.c")
-  set(HARDFILE_C "boards/${BOARD}/hardfault.c")
-  set(STUB_C     "boards/${BOARD}/libc_init_stub.c")
+function(add_stm32_example TARGET_NAME)
+    set(BOARD_DIR  ${CMAKE_CURRENT_LIST_DIR}/boards/${BOARD})
+    set(VENDOR_DIR ${BOARD_DIR}/vendor)
+    set(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/cmake/linker/stm32f410rb.ld)
 
-  # Optional linker script (set per board or globally)
-  set(LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/cmake/linker/stm32f410rb.ld" CACHE FILEPATH "STM32 linker script")
+    set(SRC_FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/${TARGET_NAME}.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/stm32_systick.cpp
+        ${BOARD_DIR}/hal_led_nucleo_f410rb.cpp
+        ${BOARD_DIR}/hardfault.c
+        ${BOARD_DIR}/libc_init_stub.c
+        ${VENDOR_DIR}/startup_stm32f410rx.s
+        ${VENDOR_DIR}/system_stm32f4xx.c
+    )
 
-  # Toolchain flags
-  set(STM32_CPU_FLAGS  "-mcpu=cortex-m4" "-mthumb")
-  set(STM32_FPU_FLAGS  "-mfpu=fpv4-sp-d16" "-mfloat-abi=softfp")
+    add_executable(${TARGET_NAME} ${SRC_FILES})
 
-  # Output filenames
-  set(BIN_FILE ${CMAKE_CURRENT_BINARY_DIR}/${EX_NAME}.bin)
-  set(HEX_FILE ${CMAKE_CURRENT_BINARY_DIR}/${EX_NAME}.hex)
-  set(MAP_FILE ${CMAKE_CURRENT_BINARY_DIR}/${EX_NAME}.map)
+    target_include_directories(${TARGET_NAME} PRIVATE
+        ${CMAKE_CURRENT_SOURCE_DIR}
+        ${BOARD_DIR}
+    )
+    target_include_directories(${TARGET_NAME} SYSTEM PRIVATE
+        ${VENDOR_DIR}
+    )
 
-  find_program(CMAKE_OBJCOPY NAMES arm-none-eabi-objcopy REQUIRED)
-  find_program(CMAKE_SIZE    NAMES arm-none-eabi-size)
 
-  add_executable(${EX_NAME}
-    ${EX_NAME}.cpp
-    ${HAL_CPP}
-    ${SYSTEM_C}
-    ${STARTUP_ASM}
-    ${HARDFILE_C}
-    ${STUB_C}
-    $<TARGET_OBJECTS:stm32_shared>
-  )
+    target_compile_definitions(${TARGET_NAME} PRIVATE
+        STM32F410Rx
+        STM32F410xx
+    )
 
-  target_include_directories(${EX_NAME} PRIVATE
-    ${CMAKE_CURRENT_SOURCE_DIR}
-    ${CMAKE_CURRENT_SOURCE_DIR}/boards/${BOARD}
-    ${CMAKE_CURRENT_SOURCE_DIR}/boards/${BOARD}/vendor
-    ${CMAKE_SOURCE_DIR}/include
-  )
+    set(STM32_CPU_FLAGS -mcpu=cortex-m4 -mthumb)
+    set(STM32_FPU_FLAGS -mfpu=fpv4-sp-d16 -mfloat-abi=softfp)
+    set(STM32_LINK_FLAGS
+        -nostdlib
+        -Wl,--gc-sections
+        -Wl,-Map=$<TARGET_FILE_DIR:${TARGET_NAME}>/${TARGET_NAME}.map
+        -T${LINKER_SCRIPT}
+        -Wl,--start-group
+        -Wl,--end-group
+    )
 
-  target_compile_definitions(${EX_NAME} PRIVATE
-    STM32F410Rx STM32F410xx
-  )
+    target_compile_options(${TARGET_NAME} PRIVATE
+        ${STM32_CPU_FLAGS}
+        ${STM32_FPU_FLAGS}
+        $<$<COMPILE_LANGUAGE:ASM>:-x assembler-with-cpp>
+    )
 
-  target_compile_options(${EX_NAME} PRIVATE
-    ${STM32_CPU_FLAGS}
-    ${STM32_FPU_FLAGS}
-    -fno-exceptions
-    -fno-rtti
-    -fno-unwind-tables
-    -fno-asynchronous-unwind-tables
-    $<$<COMPILE_LANGUAGE:ASM>:-x assembler-with-cpp>
-  )
+    target_link_options(${TARGET_NAME} PRIVATE
+        ${STM32_CPU_FLAGS}
+        ${STM32_FPU_FLAGS}
+        ${STM32_LINK_FLAGS}
+    )
 
-  target_link_options(${EX_NAME} PRIVATE
-    ${STM32_CPU_FLAGS}
-    ${STM32_FPU_FLAGS}
-    -T${LINKER_SCRIPT}
-    -nostartfiles
-    -nostdlib
-    -Wl,--gc-sections
-    -Wl,-Map=${MAP_FILE}
-    -Wl,--cref
-    -Wl,--start-group
-  )
+    target_link_options(${TARGET_NAME} PRIVATE -Wl,--start-group)
+    target_link_libraries(${TARGET_NAME} PRIVATE gcc pisco_code::core_bare)
+    target_link_options(${TARGET_NAME} PRIVATE -Wl,--end-group)
 
-  target_link_libraries(${EX_NAME} PRIVATE
-    gcc
-    pisco_code::core_bare
-  )
+    add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+        COMMAND ${CMAKE_SIZE} $<TARGET_FILE:${TARGET_NAME}>
+        COMMAND ${CMAKE_OBJCOPY} -O ihex   $<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.hex
+        COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.bin
+        COMMENT "Size and artifacts"
+    )
 
-  target_link_options(${EX_NAME} PRIVATE -Wl,--end-group)
-
-  # BIN + HEX + size
-  add_custom_command(TARGET ${EX_NAME} POST_BUILD
-    COMMAND ${CMAKE_SIZE} $<TARGET_FILE:${EX_NAME}>
-    COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${EX_NAME}> ${BIN_FILE}
-    COMMAND ${CMAKE_OBJCOPY} -O ihex   $<TARGET_FILE:${EX_NAME}> ${HEX_FILE}
-    COMMENT "Generating .bin/.hex and size report for ${EX_NAME}"
-  )
-
-  # Optional ST-Link upload via OpenOCD
-  set(OPENOCD_INTERFACE "interface/stlink.cfg" CACHE STRING "OpenOCD interface cfg")
-  set(OPENOCD_TARGET    "target/stm32f4x.cfg"  CACHE STRING "OpenOCD target cfg")
-
-  add_custom_target(${EX_NAME}_upload
-    COMMAND ${CMAKE_COMMAND} -E echo "Uploading ${EX_NAME} via OpenOCD..."
-    COMMAND openocd -f ${OPENOCD_INTERFACE} -f ${OPENOCD_TARGET}
-      -c "program $<TARGET_FILE:${EX_NAME}> verify reset exit"
-    DEPENDS ${EX_NAME}
-    USES_TERMINAL
-    COMMENT "Flashing ${EX_NAME} via ST-Link (OpenOCD)"
-  )
+    add_custom_target(${TARGET_NAME}_upload
+        COMMAND ${CMAKE_COMMAND} -E echo "Programming with OpenOCD..."
+        COMMAND openocd -f ${OPENOCD_INTERFACE} -f ${OPENOCD_TARGET}
+                -c "program $<TARGET_FILE:${TARGET_NAME}> verify reset exit"
+        DEPENDS ${TARGET_NAME}
+        USES_TERMINAL
+        COMMENT "Uploading ${TARGET_NAME} via OpenOCD"
+    )
 endfunction()
