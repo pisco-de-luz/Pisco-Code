7615624678409caff1ed8d6a2199dcf9a843b489
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-09-24 15:03:05 +0200

    Refactor basic example: update LED control timing and repeat settings for improved functionality

--- examples/avr/basic_example.cpp
+++ examples/avr/basic_example.cpp
@@ -2,23 +2,20 @@
 #include "hal_led.hpp"
 #include "pisco_code.hpp"
 
-using namespace pisco_code;
-
 int
 main()
 {
     avr_systick::init_1ms();
     hal_led::init();
-    SoftwarePwmLedController controller_led1(hal_led::Led1);
-    SignalEmitter            emitter_led1(&controller_led1);
+
+    pisco_code::SoftwarePwmLedController controller_led1(hal_led::Led1);
+    pisco_code::SignalEmitter            emitter_led1(&controller_led1);
 
     emitter_led1.setDimmedLevel(3);
 
-    const RepeatTimes repeats{3};
-    const NumDigits   num_digits{0};
-    const SignalCode  signal_code{-102};
+    emitter_led1.showCode(SignalCode{123}, NumberBase::DEC, NumDigits{0},
+                          RepeatTimes{2});
 
-    emitter_led1.showCode(signal_code, NumberBase::DEC, num_digits, repeats);
     while (emitter_led1.isRunning())
     {
         emitter_led1.loop();
@@ -27,6 +24,4 @@ main()
     for (;;)
     {
     }
-
-    return 0;
 }
--- examples/stm32/basic_example.cpp
+++ examples/stm32/basic_example.cpp
@@ -7,23 +7,20 @@ main()
 {
     stm32_systick::init_1ms();
     hal_led::init();
-    hal_led::Led1(LedControlCode::ON);
-    stm32_systick::delay_ms(1000);
-    hal_led::Led1(LedControlCode::OFF);
 
     pisco_code::SoftwarePwmLedController controller_led1(hal_led::Led1);
     pisco_code::SignalEmitter            emitter_led1(&controller_led1);
 
     emitter_led1.setDimmedLevel(3);
     emitter_led1.showCode(SignalCode{123}, NumberBase::DEC, NumDigits{0},
-                          RepeatTimes{1});
+                          RepeatTimes{2});
 
     while (emitter_led1.isRunning())
     {
         emitter_led1.loop();
         stm32_systick::delay_ms(1);
     }
-    while (1)
+    for (;;)
     {
     }
 }
