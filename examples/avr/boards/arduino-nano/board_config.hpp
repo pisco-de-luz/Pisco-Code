70365e1f5906ebb42a7c8fcdc929633d759787e1
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-09-24 14:41:49 +0200

    Refactor Arduino Nano board configuration: update LED pin definitions for clarity and correct functionality

--- examples/avr/boards/arduino-nano/board_config.hpp
+++ examples/avr/boards/arduino-nano/board_config.hpp
@@ -5,6 +5,18 @@
 #endif
 
 #define AVR_SYSTICK_USE_TIMER1 1
-#define NANO_LED_PORT PORTB
-#define NANO_LED_DDR DDRB
-#define NANO_LED_BIT 5
+
+// // TX Pin for Serial1 is D1 (PD1)
+// #define NANO_LED_PORT PORTD
+// #define NANO_LED_DDR DDRD
+// #define NANO_LED_BIT 1
+
+// RX Pin for Serial1 is D0 (PD0)
+#define NANO_LED_PORT PORTD
+#define NANO_LED_DDR DDRD
+#define NANO_LED_BIT 0
+
+// // On Arduino Nano, the built-in LED is on digital pin 13, which is PORTB5
+// #define NANO_LED_PORT PORTB
+// #define NANO_LED_DDR DDRB
+// #define NANO_LED_BIT 5
