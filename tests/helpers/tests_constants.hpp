d0288cec5a92d3c7d64db6af351b76febed3871c
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-09-28 07:46:19 +0200

    Add CODE_NEG_103 constant

--- tests/helpers/tests_constants.hpp
+++ tests/helpers/tests_constants.hpp
@@ -15,11 +15,14 @@ namespace testutils
     inline const TraceCode LED_DIMMED_CHARACTER = "-";
 
     // Duration-based repetition rules
-    inline const std::array<TraceRepeatRule, 3> TRACE_REPEAT_RULES{{{0, 1}, {1101, 3}, {2201, 5}}};
+    inline const std::array<TraceRepeatRule, 3> TRACE_REPEAT_RULES{
+        {{0, 1}, {1101, 3}, {2201, 5}}
+    };
 
     // Commonly used trace patterns
     inline constexpr SignalCode DEFAULT_CODE                     = 102;
     inline constexpr SignalCode CODE_NEG_7                       = -7;
+    inline constexpr SignalCode CODE_NEG_103                     = -103;
     inline constexpr SignalCode CODE_BIN_NEG_7                   = -7;
     inline constexpr SignalCode CODE_0                           = 0;
     inline constexpr SignalCode CODE_2                           = 2;
@@ -35,32 +38,32 @@ namespace testutils
     inline constexpr SignalCode CODE_12345                       = 12345;
     inline constexpr SignalCode CODE_12345_TRUNCATED_TO_2_DIGITS = 12345;
 
-    constexpr LedLevel LOWEST_DIMMED_LEVEL   = 1;
-    constexpr LedLevel HIGHEST_DIMMED_LEVEL  = PWM_MAX - MIN_PULSE_DIMMED_GAP;
-    constexpr LedLevel MID_DIMMED_LEVEL      = (HIGHEST_DIMMED_LEVEL - LOWEST_DIMMED_LEVEL) / 2;
+    constexpr LedLevel LOWEST_DIMMED_LEVEL  = 1;
+    constexpr LedLevel HIGHEST_DIMMED_LEVEL = PWM_MAX - MIN_PULSE_DIMMED_GAP;
+    constexpr LedLevel MID_DIMMED_LEVEL =
+        (HIGHEST_DIMMED_LEVEL - LOWEST_DIMMED_LEVEL) / 2;
     constexpr LedLevel TOO_HIGH_DIMMED_LEVEL = 255;
     constexpr LedLevel TOO_LOW_DIMMED_LEVEL  = 0;
 
-    constexpr std::array<LedLevel, 5> ALL_DIMMED_LEVELS = {{
-        LOWEST_DIMMED_LEVEL,
-        HIGHEST_DIMMED_LEVEL,
-        MID_DIMMED_LEVEL,
-        TOO_HIGH_DIMMED_LEVEL,
-        TOO_LOW_DIMMED_LEVEL,
-    }};
+    constexpr std::array<LedLevel, 5> ALL_DIMMED_LEVELS = {
+        {
+         LOWEST_DIMMED_LEVEL, HIGHEST_DIMMED_LEVEL,
+         MID_DIMMED_LEVEL, TOO_HIGH_DIMMED_LEVEL,
+         TOO_LOW_DIMMED_LEVEL, }
+    };
 
-    constexpr LedLevel LOWEST_PULSE_LEVEL   = 1;
-    constexpr LedLevel HIGHEST_PULSE_LEVEL  = PWM_MAX;
-    constexpr LedLevel MID_PULSE_LEVEL      = (HIGHEST_PULSE_LEVEL - LOWEST_PULSE_LEVEL) / 2;
+    constexpr LedLevel LOWEST_PULSE_LEVEL  = 1;
+    constexpr LedLevel HIGHEST_PULSE_LEVEL = PWM_MAX;
+    constexpr LedLevel MID_PULSE_LEVEL =
+        (HIGHEST_PULSE_LEVEL - LOWEST_PULSE_LEVEL) / 2;
     constexpr LedLevel TOO_HIGH_PULSE_LEVEL = 255;
     constexpr LedLevel TOO_LOW_PULSE_LEVEL  = 0;
 
-    constexpr std::array<LedLevel, 5> ALL_PULSE_LEVELS = {{
-        LOWEST_PULSE_LEVEL,
-        HIGHEST_PULSE_LEVEL,
-        MID_PULSE_LEVEL,
-        TOO_HIGH_PULSE_LEVEL,
-        TOO_LOW_PULSE_LEVEL,
-    }};
+    constexpr std::array<LedLevel, 5> ALL_PULSE_LEVELS = {
+        {
+         LOWEST_PULSE_LEVEL, HIGHEST_PULSE_LEVEL,
+         MID_PULSE_LEVEL, TOO_HIGH_PULSE_LEVEL,
+         TOO_LOW_PULSE_LEVEL, }
+    };
 
 } // namespace testutils
