71ac2ede64199d0b40a2f3fdef6024d378c860ed
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-09-29 16:29:58 +0200

    Add 'U' suffix to integer constants for consistency

--- tests/helpers/tests_constants.hpp
+++ tests/helpers/tests_constants.hpp
@@ -16,7 +16,7 @@ namespace testutils
 
     // Duration-based repetition rules
     inline const std::array<TraceRepeatRule, 3> TRACE_REPEAT_RULES{
-        {{0, 1}, {1101, 3}, {2201, 5}}
+        {{0U, 1U}, {1101U, 3U}, {2201U, 5U}}
     };
 
     // Commonly used trace patterns
@@ -38,13 +38,13 @@ namespace testutils
     inline constexpr SignalCode CODE_12345                       = 12345;
     inline constexpr SignalCode CODE_12345_TRUNCATED_TO_2_DIGITS = 12345;
 
-    constexpr IntensityLevel LOWEST_BASE_LEVEL = 1;
+    constexpr IntensityLevel LOWEST_BASE_LEVEL = 1U;
     constexpr IntensityLevel HIGHEST_BASE_LEVEL =
         PWM_MAX - MIN_INTENSITY_DIFFERENCE;
     constexpr IntensityLevel MID_BASE_LEVEL =
-        (HIGHEST_BASE_LEVEL - LOWEST_BASE_LEVEL) / 2;
-    constexpr IntensityLevel TOO_HIGH_BASE_LEVEL = 255;
-    constexpr IntensityLevel TOO_LOW_BASE_LEVEL  = 0;
+        (HIGHEST_BASE_LEVEL - LOWEST_BASE_LEVEL) / 2U;
+    constexpr IntensityLevel TOO_HIGH_BASE_LEVEL = 255U;
+    constexpr IntensityLevel TOO_LOW_BASE_LEVEL  = 0U;
 
     constexpr std::array<IntensityLevel, 5> ALL_BASE_LEVELS = {
         {
@@ -53,12 +53,12 @@ namespace testutils
          TOO_LOW_BASE_LEVEL, }
     };
 
-    constexpr IntensityLevel LOWEST_PULSE_LEVEL  = 1;
+    constexpr IntensityLevel LOWEST_PULSE_LEVEL  = 1U;
     constexpr IntensityLevel HIGHEST_PULSE_LEVEL = PWM_MAX;
     constexpr IntensityLevel MID_PULSE_LEVEL =
-        (HIGHEST_PULSE_LEVEL - LOWEST_PULSE_LEVEL) / 2;
-    constexpr IntensityLevel TOO_HIGH_PULSE_LEVEL = 255;
-    constexpr IntensityLevel TOO_LOW_PULSE_LEVEL  = 0;
+        (HIGHEST_PULSE_LEVEL - LOWEST_PULSE_LEVEL) / 2U;
+    constexpr IntensityLevel TOO_HIGH_PULSE_LEVEL = 255U;
+    constexpr IntensityLevel TOO_LOW_PULSE_LEVEL  = 0U;
 
     constexpr std::array<IntensityLevel, 5> ALL_PULSE_LEVELS = {
         {
