d557ed847bcd3cb71caea0cfdcf10666248ea1bb
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-10-01 09:32:19 +0200

    Refactor basic_example to initialize both onboard and hardware PWM LED controllers

--- examples/avr/basic_example.cpp
+++ examples/avr/basic_example.cpp
@@ -6,18 +6,28 @@ int
 main()
 {
     avr_systick::init_1ms();
-    hal_led::init();
+    hal_led::ledOnboardInit();
+    hal_led::ledHwPwmInit();
 
-    pisco_code::SoftwarePwmLedController controller_led1(hal_led::led1);
-    pisco_code::SignalEmitter            emitter_led1(&controller_led1);
+    // pisco_code::SoftwarePwmLedController controller_led1(hal_led::ledHwPwm);
+    pisco_code::SoftwarePwmLedController controller_led1(hal_led::ledOnboard);
+    pisco_code::HardwarePwmLedController controller_led2(
+        hal_led::ledHwPwmSetLevel);
+    pisco_code::SignalEmitter emitter_led1(&controller_led1);
+    pisco_code::SignalEmitter emitter_led2(&controller_led2);
 
     emitter_led1.setBaseLevel(3);
     emitter_led1.setRepeatTimes(2);
     emitter_led1.showCode(SignalCode{123}, NumberBase::DEC, NumDigits{0});
 
-    while (emitter_led1.isRunning())
+    emitter_led2.setBaseLevel(3);
+    emitter_led2.setRepeatTimes(2);
+    emitter_led2.showCode(SignalCode{-103}, NumberBase::DEC, NumDigits{0});
+
+    while (emitter_led1.isRunning() || emitter_led2.isRunning())
     {
         emitter_led1.loop();
+        emitter_led2.loop();
         avr_systick::delay_ms(1);
     }
     for (;;)
