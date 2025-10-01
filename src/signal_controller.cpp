f7d48c1e9f62d998c111835c947a48e3637c1769
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2025-10-01 15:12:54 +0200

    Refactor setHighLevel and setLowLevel methods to ensure minimum intensity difference is maintained

--- src/signal_controller.cpp
+++ src/signal_controller.cpp
@@ -1,15 +1,35 @@
 #include "signal_controller.hpp"
 
+#include "pisco_constants.hpp"
+#include "pisco_types.hpp"
+#include "signal_types.hpp"
+
 namespace pisco_code
 {
     void SignalController::setHighLevel(IntensityLevel level)
     {
-        high_level_ = level;
+        high_level_ = (level < MIN_INTENSITY_DIFFERENCE)
+                          ? MIN_INTENSITY_DIFFERENCE
+                          : level;
+
+        // Ensure low/high level maintains minimum difference
+        if (low_level_ > high_level_ - MIN_INTENSITY_DIFFERENCE)
+        {
+            low_level_ = (high_level_ >= MIN_INTENSITY_DIFFERENCE)
+                             ? high_level_ - MIN_INTENSITY_DIFFERENCE
+                             : 0;
+        }
     }
 
     void SignalController::setLowLevel(IntensityLevel level)
     {
-        low_level_ = level;
+        low_level_ = (level > MAX_LOW_LEVEL) ? MAX_LOW_LEVEL : level;
+
+        // Ensure low/high level maintains minimum difference
+        if (high_level_ < low_level_ + MIN_INTENSITY_DIFFERENCE)
+        {
+            high_level_ = low_level_ + MIN_INTENSITY_DIFFERENCE;
+        }
     }
 
     void SignalController::setCurrentSignalMode(SignalMode mode)
