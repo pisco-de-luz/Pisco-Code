c8ef6552d447b361fc82cd6d6265aaa0522575f5
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2026-03-02 23:11:27 +0100

    Move Arduino distribution to separate repo, add auto-publish job

--- src/Pisco-Code.h
+++
@@ -1,9 +0,0 @@
-// Arduino-compatible wrapper header for Pisco Code library.
-// This file includes the main library header from the include/ directory.
-
-#ifndef PISCO_CODE_ARDUINO_H
-#define PISCO_CODE_ARDUINO_H
-
-#include "../include/pisco_code.hpp"
-
-#endif // PISCO_CODE_ARDUINO_H
